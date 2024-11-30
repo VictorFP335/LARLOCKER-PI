a26de5c059e92641a385da7a1841212487e20cc4
Yan Yoshida <87395020+Yagami5735@users.noreply.github.com>
2024-11-30 20:26:47 -0300

    modo claro funcionando

--- static/js/script_comodo.js
+++ static/js/script_comodo.js
@@ -269,14 +269,21 @@ async function updateQuantity(row, change) {
         }
     }
 }
-/* Modo Claro/Escuro */
-const chk = document.getElementById('chk')
+
+const chk = document.getElementById('chk');
+const imagem = document.getElementById('imagem');
 
 chk.addEventListener('change', () => {
-    document.body.classList.toggle('white');
-    const imagem = document.getElementById('imagem');
-    imagem.setAttribute('src', "static/img/larlocker_branco.png")
-})
+  document.body.classList.toggle('white'); // Alterna o modo claro/escuro
+
+  // Alterna a imagem da logo com base no modo
+  if (document.body.classList.contains('white')) {
+    imagem.setAttribute('src', 'static/img/larlocker_branco.png'); // Logo modo claro
+  } else {
+    imagem.setAttribute('src', 'static/img/larlocker.png'); // Logo modo escuro
+  }
+});
+
 
 // Fecha o modal quando o usuário clicar fora dele
 window.onclick = function (event) {
